21dc30437775a987115611c7c27aef89af1f6e1e
LucasGarciag <83224887+LucasGarciag@users.noreply.github.com>
2022-05-25 14:33:13 -0300

    Add files via upload

---
+++ src/main/java/com/example/demo/model/Car.java
@@ -0,0 +1,12 @@
+package com.example.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Car {
+    private String brand;
+    private String year;
+    private String model;
+}
